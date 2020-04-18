e7ec71e478cf4e091be3f9bbc14020057a661232
Eugeniy <freelans73@gmail.com>
2020-04-18 19:01:19 +0400

    Initial Commit

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly DataContext _context;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, DataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Value>>> Get(int id)
+        {
+            var value = await _context.Values.FindAsync(id);
+            System.Console.WriteLine(value.Id);
+            return Ok(value);
+        }
+    }
+}
