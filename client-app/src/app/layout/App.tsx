720fd81ee5f5f79317bddf6319d8172049ab09ad
MetalGoose <freelans73@gmail.com>
2020-06-03 18:20:41 +0400

    End of section 7

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,107 +1,37 @@
-import React, { useState, useEffect, Fragment, SyntheticEvent } from "react";
+import React, { useEffect, Fragment, useContext } from "react";
 import { Container } from "semantic-ui-react";
-import { IActivity } from "../models/activity";
-import { NavBar } from "../../features/nav/NavBar";
-import { ActivityDashBoard } from "../../features/activities/dashboard/ActivityDashBoard";
-import agent from "../api/agent";
+import NavBar from "../../features/nav/NavBar";
 import { LoadingComponent } from "./LoadingComponent";
+import ActivityStore from '../stores/activityStore';
+import { observer } from "mobx-react-lite";
+import ActivityDashBoard from "../../features/activities/dashboard/ActivityDashBoard";
 
 const App = () => {
 
   //#region states
 
-  const [activities, setActivities] = useState<IActivity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
-  const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [submitting, setSubmitting] = useState(false); //Индикатор загрузки при нажатии на кнопку
-  const [target, setTarget] = useState('');
+  const activityStore = useContext(ActivityStore)
 
   //#endregion
 
-  const handleOpenCreateForm = () => {
-    setSelectedActivity(null);
-    setEditMode(true);
-  }
-
-  /**
-   * Хранит в себе выбранную активность
-   * @param id of selected activity
-   */
-  const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.filter((a) => a.id === id)[0]);
-    setEditMode(false);
-  };
-
-  const handleCreateActivity = (activity: IActivity) => {
-    setSubmitting(true);
-    agent.Activities.create(activity).then(() => {
-      setActivities([...activities, activity]); // Добавляем в массив старые активности + новую
-      setEditMode(false);
-      setSelectedActivity(activity);
-    }).then(() => setSubmitting(false));
-  }
-
-  const handleEditActivity = (activity: IActivity) => {
-    setSubmitting(true);
-    agent.Activities.update(activity).then(() => {
-      setActivities([...activities.filter(a => a.id !== activity.id), activity]); // Обновляем массив акивностей 
-      setEditMode(false);
-      setSelectedActivity(activity);
-    }).then(() => setSubmitting(false));
-  }
-
-  /**
-   * Возвращает все активности кроме указанной
-   * @param id id активности которую требуется удалить
-   */
-  const handleDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>,id: string) => {
-    setSubmitting(true);
-    setTarget(event.currentTarget.name);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(a => a.id !== id)]);
-    }).then(() => setSubmitting(false));
-  }
-
-
   /**
    * useEffect Вызывается всякий раз когда компонент рендерится
-   * второй параметр для того что бы вызов происходил только один раз - по сути это аналогаично вызову componentDidMount
+   * второй параметр (в случае если это пустой массив) для того что бы вызов происходил только один раз - по сути это аналогаично вызову componentDidMount
    */ 
   useEffect(() => {
-    agent.Activities.list()
-      .then((response) => {
-        let activities: IActivity[] = [];
-        response.forEach(activity => {
-          activity.date = activity.date.split('.')[0]; // API возвращает дату слишком точно. Урезаем ее до секунд
-          activities.push(activity);
-        });
-        setActivities(activities);
-      }).then(() => setLoading(false));
-  }, []);
+    activityStore.loadActivities();
+  }, [activityStore]);
 
-  if (loading) return <LoadingComponent content={"Loading activities..."}/>
+  if (activityStore.loadingInitial) return <LoadingComponent content={"Loading activities..."}/>
 
   return (
     <Fragment>
-      <NavBar openCreateForm={handleOpenCreateForm}/>
+      <NavBar/>
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashBoard
-          activities={activities}
-          selectActivity={handleSelectActivity}
-          selectedActivity={selectedActivity}
-          editMode={editMode}
-          setEditMode={setEditMode}
-          setSelectedActivity={setSelectedActivity}
-          createActivity={handleCreateActivity}
-          editActivity={handleEditActivity}
-          deleteActivity={handleDeleteActivity}
-          submitting={submitting}
-          target={target}
-        />
+        <ActivityDashBoard />
       </Container>
     </Fragment>
   );
 };
 
-export default App;
+export default observer(App);
---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,133 @@
+import { observable, action, computed, configure, runInAction } from 'mobx'
+import { createContext, SyntheticEvent } from 'react';
+import { IActivity } from '../models/activity';
+import agent from '../api/agent';
+
+configure({enforceActions: 'always'});
+
+class ActivityStore {
+    @observable activityRegistry = new Map();
+    @observable activities: IActivity[] = [];
+    @observable selectedActivity: IActivity | undefined;
+    @observable loadingInitial = false;
+    @observable editMode = false;
+    @observable submitting = false; //Индикатор загрузки при нажатии на кнопку
+    @observable target = '';
+
+    @computed get activitiesByDate() : IActivity[] {
+        return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+    }
+    
+
+    @action loadActivities = async () => {
+        this.loadingInitial = true;
+        //async/await способ
+        try {
+            const activities = await agent.Activities.list();
+            runInAction('Loading activities',() => { // Первый параметр - имя действия. Не обязательно, но может помочь в Mobx dev tools
+                activities.forEach(activity => {
+                    activity.date = activity.date.split('.')[0]; // API возвращает дату слишком точно. Урезаем ее до секунд
+                    this.activityRegistry.set(activity.id, activity);
+                });
+            });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            runInAction('Loading activities finished',() => this.loadingInitial = false);
+        }
+        //promise chain способ
+        // agent.Activities.list()
+        //     .then((activities) => {
+        //         activities.forEach(activity => {
+        //         activity.date = activity.date.split('.')[0]; // API возвращает дату слишком точно. Урезаем ее до секунд
+        //         this.activities.push(activity);
+        //         });
+        //     })
+        //     .catch((error) => console.log(error))
+        //     .finally(() => this.loadingInitial = false); // finally - выполнится даже если промис будет не выполнен
+    }
+
+    @action createActivity = async (activity: IActivity) => {
+        this.submitting = true;
+        try {
+            await agent.Activities.create(activity);
+            runInAction('Create activity',() => {
+                this.activityRegistry.set(activity.id, activity);
+                this.editMode = false;
+            });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            runInAction('Create activity finished',() => {
+                this.submitting = false;
+            });
+        }
+    }
+
+    @action editActivity = async (activity: IActivity) => {
+        this.submitting = true;
+        try {
+            await agent.Activities.update(activity);
+            runInAction('Edit activity',() => {
+                this.activityRegistry.set(activity.id, activity); // Updating activity by id
+                this.selectedActivity = activity;
+                this.editMode = false;
+            });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            runInAction('Edit activity finished',() => {
+                this.submitting = false;
+            });
+        }
+    }
+
+    @action deleteActivity = async (event: SyntheticEvent<HTMLButtonElement>,id: string) => {
+        this.submitting = true;
+        this.target = event.currentTarget.name;
+        try {
+            await agent.Activities.delete(id);
+            runInAction('Delete activity', () => {
+                this.activityRegistry.delete(id);
+            });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            runInAction('Delete activity finished', () => {
+                this.submitting = false;
+                this.target = '';
+            });
+        }
+    }
+
+     /**
+     * Открываем форму редактирования активности
+     */
+    @action openEditForm = (id: string) => {
+        this.selectedActivity = this.activityRegistry.get(id);
+        this.editMode = true;
+    }
+
+    @action cancelSelectedActivity = () => {
+        this.selectedActivity = undefined;
+    }
+
+    @action cancelOpenForm = () => {
+        this.editMode = false;
+    }
+
+    /**
+     * Открываем форму создания активности
+     */
+    @action openCreateForm = () => {
+        this.editMode = true;
+        this.selectedActivity = undefined;
+    }
+
+    @action selectActivity = (id: string) => {
+        this.selectedActivity = this.activityRegistry.get(id);
+        this.editMode = false;
+    }
+}
+
+export default createContext(new ActivityStore())
