eafdad1e5f28cb661e662bccab6bccbb4d95647b
MetalGoose <freelans73@gmail.com>
2020-07-26 21:16:34 +0400

    Add grid into Activity form page and do some cleanup.

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect } from "react";
-import { Card, Image, Button, Grid } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import ActivityStore from "../../../app/stores/activityStore";
 import { observer } from "mobx-react-lite";
-import { RouteComponentProps, Link } from "react-router-dom";
+import { RouteComponentProps } from "react-router-dom";
 import { LoadingComponent } from "../../../app/layout/LoadingComponent";
 import ActivityDetailsHeader from "./ActivityDetailsHeader";
 import { ActivityDetailsInfo } from "./ActivityDetailsInfo";
@@ -14,9 +14,7 @@ interface IDetailParams {
 }
 
 export const ActivityDetails: React.FC<RouteComponentProps<IDetailParams>> = ({
-  match,
-  history
-}) => {
+  match}) => {
   const activityStore = useContext(ActivityStore);
   const {
     selectedActivity,
