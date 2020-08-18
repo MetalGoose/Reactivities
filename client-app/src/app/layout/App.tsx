50c535ec329cc6da7940c538195d579949a64d03
MetalGoose <freelans73@gmail.com>
2020-08-18 15:06:24 +0400

    Add NotFound component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,18 @@
 import { observer } from "mobx-react-lite";
 import React, { Fragment } from "react";
-import { Route, withRouter, RouteComponentProps } from "react-router-dom";
+import {
+  Route,
+  withRouter,
+  RouteComponentProps,
+  Switch,
+} from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import ActivityDashBoard from "../../features/activities/dashboard/ActivityDashBoard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import { HomePage } from "../../features/home/HomePage";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import NotFound from "./NotFound";
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
   return (
@@ -18,13 +24,17 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
           <Fragment>
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
-              <Route exact path="/activities" component={ActivityDashBoard} />
-              <Route path="/activities/:id" component={ActivityDetails} />
-              <Route
-                key={location.key}
-                path={["/createActivity", "/manage/:id"]}
-                component={ActivityForm}
-              />
+              {/* Switch следит за тем, что бы отображался только один путь. Тот который был выбран */}
+              <Switch>
+                <Route exact path="/activities" component={ActivityDashBoard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={["/createActivity", "/manage/:id"]}
+                  component={ActivityForm}
+                />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </Fragment>
         )}
